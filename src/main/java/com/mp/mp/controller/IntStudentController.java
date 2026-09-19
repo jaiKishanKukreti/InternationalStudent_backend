4607d7a290c09ec73f1204b1d5e356989704313d
jaiKishanKukreti <jaikukreti93@gmail.com>
2026-09-19 23:26:38 +0530

    Update IntStudentController.java

--- src/main/java/com/mp/mp/controller/IntStudentController.java
+++ src/main/java/com/mp/mp/controller/IntStudentController.java
@@ -12,7 +12,7 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/student")
-@CrossOrigin(origins = "http://localhost:5173")
+@CrossOrigin(origins = "https://international-student-frontend.vercel.app/")
 public class IntStudentController {
 
     @Autowired
